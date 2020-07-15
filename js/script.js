f44a7ee7e5de06a00f1cadb02f68e1a709bfd5f4
Valerio <valerio.modesti@gmail.com>
2020-07-15 17:47:18 +0200

    js-paliedispari

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
 // Chiedere all'utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+//versione senza funzione
+
+// var parola = prompt('Inserisci una parola');
+// var contrario = '';
+//
+// for (var i = parola.length; i >= 0 ; i--){
+//     contrario += parola.charAt(i);
+// }
+//
+// if(parola == contrario){
+//     console.log('La parola è palindroma');
+// } else {
+//      console.log('La parola non è palindroma');
+// }
+
+
+// creazione funzione
+
 var parola = prompt('Inserisci una parola');
-var contrario = '';
 
-for (var i = parola.length; i >= 0 ; i--){
-    contrario += parola.charAt(i);
-}
-console.log(contrario);
+var risultato = isPalindrome(parola);
+console.log(risultato);
 
-if(parola == contrario){
+if(parola == risultato){
     console.log('La parola è palindroma');
 } else {
      console.log('La parola non è palindroma');
 }
 
-// creazione funzione
+
+
+function isPalindrome(stringa){
+    var contrario = '';
+
+    for (var i = parola.length; i >= 0 ; i--){
+        contrario += parola.charAt(i);
+    }
+
+    return contrario;
+}
